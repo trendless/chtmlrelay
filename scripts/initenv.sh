ed93678c9dd1a67efb2bddebee9c73e2f9a04488
missytake <missytake@systemli.org>
2025-04-25 19:38:44 +0200

    cmdeploy: on ubuntu/debian, test if python3-dev is installed

--- scripts/initenv.sh
+++ scripts/initenv.sh
@@ -1,5 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 set -e
+
+if [[ $(lsb_release -is 2> /dev/null) == "Ubuntu" || $(lsb_release -is 2> /dev/null) == "Debian" ]]
+then
+  if [ $(dpkg -l | grep python3-dev 2>&1 /dev/null) ]
+  then
+    echo "You need to install python3-dev for installing the other dependencies."
+    exit 1
+  fi
+fi
+
 python3 -m venv --upgrade-deps venv
 
 venv/bin/pip install -e chatmaild 
