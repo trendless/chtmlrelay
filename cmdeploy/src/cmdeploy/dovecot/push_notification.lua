e0140bbad5b3087b7fbfcd3f2de4e34bb7da69d9
Sandra Snan <sandra.snan@idiomdrottning.org>
2025-05-25 20:19:30 +0200

    Remove contains from lua
    Is this function even doing anything? If so reject PR. I'm still
    trying to understand the code.

--- cmdeploy/src/cmdeploy/dovecot/push_notification.lua
+++ cmdeploy/src/cmdeploy/dovecot/push_notification.lua
@@ -2,15 +2,6 @@ function dovecot_lua_notify_begin_txn(user)
   return user
 end
 
-function contains(v, needle)
-  for _, keyword in ipairs(v) do
-    if keyword == needle then
-      return true
-    end
-  end
-  return false
-end
-
 function dovecot_lua_notify_event_message_new(user, event)
   local mbox = user:mailbox(event.mailbox)
   mbox:sync()
